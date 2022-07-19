4bbf12f716df3cadb1428fbcc0f2bff17f6d1f32
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-07-19 11:15:28 -0400

    cleanup

--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -47,8 +48,27 @@ public class UserController {
 	@PutMapping("/update")
 	public ResponseEntity<?> updateUser(@Valid @RequestBody User user) throws Exception {
 		Integer id = user.getId();
-		int status = userRepo.findById(id).isEmpty() ? 201 : 200;
-		User updated = userRepo.save(user);
-		return ResponseEntity.status(status).body(user);
+		if(userRepo.findById(id).isEmpty()) {
+			return CreateUser(user);
+		}
+		user.setPassword(encoder.encode(user.getPassword()));
+		return ResponseEntity.status(200).body(userRepo.save(user));
+	}
+	@DeleteMapping("/delete")
+	public ResponseEntity<?> removeUser(@RequestBody User user) throws Exception {
+	        String name = user.getUsername();
+	        Optional<User> deleted = userRepo.findByUsername(name);
+	        if (deleted.isEmpty()) {
+	            throw new Exception("User " + name + " not found.");
+	        }
+	        userRepo.deleteById(deleted.get().getId());
+	        return ResponseEntity.status(200).body(deleted.get());
+	    }
+	@DeleteMapping("/delete/me")
+	public ResponseEntity<?> deleteMyUser(Authentication authentication, 
+			Principal principal) throws Exception {
+		User me = userRepo.findByUsername(authentication.getName()).get();
+		userRepo.deleteById(me.getId());
+		return ResponseEntity.status(200).body(me);
 	}
 }

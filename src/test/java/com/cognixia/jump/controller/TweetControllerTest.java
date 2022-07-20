8c5c0cd9594eef63e65094df81210f9d8233f479
Unnati Patel <unnati5063@gmail.com>
2022-07-20 14:31:55 -0500

    conforming with a main

---
+++ src/test/java/com/cognixia/jump/controller/TweetControllerTest.java
@@ -0,0 +1,14 @@
+package com.cognixia.jump.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TweetControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

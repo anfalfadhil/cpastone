6af336792c93f177796ddc495e4151550e381c61
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-07-21 14:59:16 -0400

    merge ready

--- src/test/java/com/cognixia/jump/controller/UserControllerTest.java
+++ src/test/java/com/cognixia/jump/controller/UserControllerTest.java
@@ -15,5 +15,31 @@ import com.cognixia.jump.model.User;
 
 class UserControllerTest {
 
+	@Test
+	void testLogin() {
+		InputStream stdin = System.in;
+		System.setIn(new ByteArrayInputStream("default\n1234\n".getBytes()));
+		
+		
+		PrintStream originalOut = System.out;
+        OutputStream os = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(os);
+        System.setOut(ps);
+        
+        try {
+			User user = new User();
+			
+			
+			assertTrue(user.getUsername().equals("default"), "Login successfull");
+			user.exit();
+			assertTrue(user.getConn().isClosed(), "Connection successfully closed");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.setIn(stdin);
+		System.setOut(originalOut);
+	}
 
 }

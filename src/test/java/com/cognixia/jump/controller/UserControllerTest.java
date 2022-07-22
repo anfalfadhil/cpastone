220466f207e533aa748a45a260fefb88e55f9a38
Unnati5063 <81659579+Unnati5063@users.noreply.github.com>
2022-07-22 11:32:32 -0500

    Update UserControllerTest.java

--- src/test/java/com/cognixia/jump/controller/UserControllerTest.java
+++ src/test/java/com/cognixia/jump/controller/UserControllerTest.java
@@ -2,18 +2,42 @@ package com.cognixia.jump.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.sql.SQLException;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.context.annotation.FilterType;
 
-import com.cognixia.jump.model.User;
+import com.cognixia.jump.config.JwtAuthenticationEntryPoint;
+import com.cognixia.jump.controller.UserController;
+import com.cognixia.jump.filter.JwtRequestFilter;
+import com.cognixia.jump.repository.UserRepository;
+import com.cognixia.jump.service.MyUserDetailsService;
+import com.cognixia.jump.util.JwtUtil;
 
-class UserControllerTest {
+@WebMvcTest(value = UserController.class, includeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtUtil.class) })
+public class UserControllerTest {
 
+	private static final String STARTING_URI = "http://localhost:8080/api";
 
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	
+	@MockBean
+	private MyUserDetailsService myUserDetailsService;
+
+	@MockBean
+	private UserRepository repo;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+	
+	@Autowired
+	private JwtRequestFilter filter;
 }
